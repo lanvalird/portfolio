4d662ea8fbefd24e2cd852d89812070678f79377
lanvalird <strazhognay@gmail.com>
2026-03-02 02:41:51 +0400

    add full static project page, add display project date

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,12 +1,16 @@
 import type { Project } from "@/shared/types";
 
 import { Badge } from "@/shared/components/ui/badge";
-import { MDXContent } from "./_ui/mdx-content";
 
-import { notFound } from "next/navigation";
 import { Button } from "@/shared/components/ui/button";
-import { Link as LinkIcon } from "lucide-react";
+import { GitBranchIcon, Link as LinkIcon } from "lucide-react";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+
+import { useMDXComponents } from "@/mdx-components";
+
+import { evaluate } from "@mdx-js/mdx";
+import * as runtime from "react/jsx-runtime";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -80,31 +84,63 @@ export default async function ProjectPage({ params }: Props) {
     return notFound();
   }
 
+  const MDXContent = await evaluate(project.markdown, {
+    ...runtime,
+    // @ts-expect-error
+    useMDXComponents,
+  }).then((mdxModule) => mdxModule.default);
+
   return (
-    <main className="mx-auto my-o px-16 py-8 max-w-5xl">
+    <main className="mx-auto my-o px-16 py-8 max-w-5xl relative">
       <h1 className="scroll-m-20 my-8 text-center text-4xl font-extrabold tracking-tight text-balance">
-        {project.name}{" "}
-        {(project.urls.homepage || project.urls.github) && (
+        {project.name}
+      </h1>
+      <div className="absolute top-4 right-8">
+        {project.urls.homepage && (
           <Button variant={"ghost"} asChild>
-            <Link href={project.urls.homepage || project.urls.github!} target="_blank" rel="noopener noreferrer">
+            <Link href={project.urls.homepage} target="_blank" rel="noopener noreferrer">
               <LinkIcon />
             </Link>
           </Button>
         )}
-      </h1>
+        {project.urls.github && (
+          <Button variant={"ghost"} asChild>
+            <Link href={project.urls.github || project.urls.github!} target="_blank" rel="noopener noreferrer">
+              <GitBranchIcon />
+            </Link>
+          </Button>
+        )}
+      </div>
 
       <p className="text-muted-foreground text-xl text-center mb-6">{project.description}</p>
 
       <div className="flex w-full justify-center flex-wrap gap-2">
-        {project?.tags.map((tag) => (
+        {project.tags.map((tag) => (
           <Badge key={tag} variant="outline">
             {tag}
           </Badge>
         ))}
       </div>
 
-      {project.markdown && <MDXContent mdx={project.markdown} />}
+      <MDXContent />
+
+      <hr className="mt-8 mb-6" />
+
+      <p className="text-muted-foreground text-base text-center mb-2">
+        {humanizeDate(new Date(project.created))} — {humanizeDate(new Date(project.updated))}
+      </p>
     </main>
   );
 }
+
+function humanizeDate(date: Date) {
+  function whenZero(num: number) {
+    return num.toString().length === 2 ? num : `0${num}`;
+  }
+  const day = whenZero(date.getUTCDate());
+  const month = whenZero(date.getUTCMonth() + 1);
+
+  return `${day}.${month}.${date.getUTCFullYear()}`;
+}
+
 export const revalidate = 36000;
