965a7036dac5b440a4da63282d4d35445f028461
lanvalird <strazhognay@gmail.com>
2026-04-25 16:27:11 +0400

    add new param (query) "categories" to `/api/skills`

--- frontend/src/app/api/[...route]/_routes/skills.ts
+++ frontend/src/app/api/[...route]/_routes/skills.ts
@@ -6,8 +6,15 @@ const route = new Hono().basePath("/skills");
 route.get("/", (c) => {
   const size = Number(c.req.query("size") || 10);
   const skip = Number(c.req.query("skip") || 0);
+  const categories = c.req.queries("categories") || [];
 
-  const filtered = skills.slice(skip, size);
+  const filtered = (
+    categories.length === 0
+      ? skills
+      : skills.filter((skill) =>
+          skill.categories.some((category) => categories.includes(category)),
+        )
+  ).slice(skip, size);
 
   return c.json(filtered);
 });
