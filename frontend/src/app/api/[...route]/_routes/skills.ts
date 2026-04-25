72e41467b7874c7a41ed31022cc3cadf828329f6
lanvalird <strazhognay@gmail.com>
2026-04-25 17:42:07 +0400

    fix `/api/skills` bug with equalents, fix display roles team

--- frontend/src/app/api/[...route]/_routes/skills.ts
+++ frontend/src/app/api/[...route]/_routes/skills.ts
@@ -6,12 +6,17 @@ const route = new Hono().basePath("/skills");
 route.get("/", (c) => {
   const size = Number(c.req.query("size") || 10);
   const skip = Number(c.req.query("skip") || 0);
-  const categories = c.req.queries("categories") || [];
-
+  const categories = (c.req.queries("categories") || []).map((category) =>
+    category.toLowerCase(),
+  );
   const filtered = (
     categories.length === 0
       ? skills
-      : skills.filter((skill) => skill.categories.some((category) => categories.includes(category)))
+      : skills.filter((skill) =>
+          skill.categories
+            .map((skillCategory) => skillCategory.toLowerCase())
+            .some((skillCategory) => categories.includes(skillCategory)),
+        )
   ).slice(skip, size);
 
   return c.json(filtered);
