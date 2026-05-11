aa6cfa92a420b4bd52b0326a9383d6d0defb05b4
lanvalird <strazhognay@gmail.com>
2026-05-11 08:42:03 +0400

    update teams and skills data (so a little bit)

--- frontend/src/shared/data-storage/skills/technologies/frontend/index.ts
+++ frontend/src/shared/data-storage/skills/technologies/frontend/index.ts
@@ -4,6 +4,16 @@ import { DEVELOPMENT_TYPE, addCategoryForEach } from "../../lib";
 import { onlyBuilder, onlyDocumentation, onlyStyling, onlyUi } from "./only";
 
 export const onlyFrontend: Skill[] = addCategoryForEach<DEVELOPMENT_TYPE>(
-  [...onlyUi, ...onlyBuilder, ...onlyStyling, ...onlyDocumentation],
+  [
+    ...onlyUi,
+    ...onlyBuilder,
+    ...onlyStyling,
+    ...onlyDocumentation,
+    {
+      name: "Storybook",
+      description: "Разработка и UI/UX-тестирование",
+      categories: ["Documantation", "UI"],
+    },
+  ],
   DEVELOPMENT_TYPE.FRONTEND,
 );
