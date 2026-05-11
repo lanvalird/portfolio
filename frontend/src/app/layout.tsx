f2027126ec6169a68f8b940acdf189219835a564
lanvalird <strazhognay@gmail.com>
2026-05-11 05:21:05 +0400

    add alert into main layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,15 +2,25 @@ import type { Metadata } from "next";
 
 import { ThemeProvider } from "@/shared/components/theme-provider";
 
-import "./globals.css";
 import { AppHeader } from "@/widgets/app-header";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+} from "@lanvalird/lxv-ui";
+import { GlassWaterIcon } from "lucide-react";
+
+import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: {
     template: "%s • lanvalird",
     default: "◊ Valentin (lanvalird) Bird ◊",
   },
-  description: "Персональный сайт Валентина Бёрда (lanvalird) - Fullstack разработчика",
+  description:
+    "Персональный сайт Валентина Бёрда (lanvalird) - Fullstack разработчика",
   keywords: [
     "web developer",
     "fullstack",
@@ -32,13 +42,32 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ru" suppressHydrationWarning>
       <head />
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <div className="fixed inset-0 -z-1000 bg-linear-to-b from-background to-secondary/20" />
+          <Item variant="muted" className="rounded-none justify-center">
+            <ItemMedia variant="icon">
+              <GlassWaterIcon />
+            </ItemMedia>
+            <ItemContent className="max-w-max">
+              <ItemDescription>
+                Чудесный день! <Link href={"/links"}>Не так ли это?</Link>
+              </ItemDescription>
+            </ItemContent>
+          </Item>
           <AppHeader />
           <main className="max-w-7xl mx-auto my-0">{children}</main>
         </ThemeProvider>
